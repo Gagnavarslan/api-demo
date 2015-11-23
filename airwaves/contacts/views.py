61f43a2fb4c0fa8d9523a16f6002095b39dc7c00
arnlaugsson <skuli.arnlaugsson@gmail.com>
2015-11-23 17:26:52 +0000

    Add .env file to store credentials
    Remove the hardcoded string 'Administrator' for a variable read from the
    .env file.

--- airwaves/contacts/views.py
+++ airwaves/contacts/views.py
@@ -1,7 +1,12 @@
+import os
 import requests
 
-from django.shortcuts import render
 from django.views.generic import TemplateView
+
+
+COREDATA_URL = os.environ.get("COREDATA_URL")
+COREDATA_USER = os.environ.get("COREDATA_USER")
+COREDATA_PASSWORD = os.environ.get("COREDATA_PASSWORD")
 
 
 class Contacts(TemplateView):
@@ -11,8 +16,8 @@ class Contacts(TemplateView):
         context = super(Contacts, self).get_context_data(**kwargs)
 
         contacts = requests.get(
-            'https://airwaves.coredata.is/api/v2/contacts',
-            auth=('Administrator', 'Administrator')
+            '{}api/v2/contacts'.format(COREDATA_URL),
+            auth=(COREDATA_USER, COREDATA_PASSWORD)
         )
         context["contacts"] = contacts.json()
 
